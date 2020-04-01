604a3d711aded7294fe63904b740879aea8d27dc
Neeraj <n.sharma1396@gmail.com>
2020-04-02 02:05:08 +0530

    Implement update details and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,6 +152,43 @@ authController.resetPassword = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+authController.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+authController.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const { protect } = require("../middlewares/auth");
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
 authRouter.get("/me", protect, authController.getMe);
+authRouter.put("/updatedetails", protect, authController.updateDetails);
+authRouter.put("/updatepassword", protect, authController.updatePassword);
 authRouter.post("/forgotpassword", authController.forgotPassword);
 authRouter.put("/resetpassword/:resetToken", authController.resetPassword);
 
