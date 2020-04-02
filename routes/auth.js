0c149e4b700d9354f6896dc87ffa6fe949f6ab16
Neeraj <n.sharma1396@gmail.com>
2020-04-02 18:25:31 +0530

    Implement logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { protect } = require("../middlewares/auth");
 
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
+authRouter.get("/logout", authController.logout);
 authRouter.get("/me", protect, authController.getMe);
 authRouter.put("/updatedetails", protect, authController.updateDetails);
 authRouter.put("/updatepassword", protect, authController.updatePassword);
